4ac1b2197533c154ea395c971e167aecfbd2f318
Gonzalo <gonchogfa@gmail.com>
2025-03-24 21:01:56 +0100

    error env wp-config.php

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv( 'DB_NAME' ) );
+define('WP_SITEURL', 'https://gfernand.42.fr/wordpress');
+define('WP_HOME', 'https://gfernand.42.fr/wordpress');
 
-/** Database username */
-define( 'DB_USER', getenv( 'DB_USER' ) );
-
-/** Database password */
-define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
-
-/** Database hostname */
-define( 'DB_HOST', getenv( 'DB_HOST' ) );
+define( 'DB_NAME', 'maria_db');
+define( 'DB_USER', 'gonzalo');
+define( 'DB_PASSWORD', '123zalo');
+define( 'DB_HOST', 'mariadb');
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,32 +8,28 @@ WORDPRESS_URL="https://wordpress.org/latest.tar.gz"
 WORDPRESS_DIR="/var/www/html/wordpress"
 WP_CONFIG_SOURCE="/wp-config.php"
 
-if [ ! -f "$WORDPRESS_DIR/index.php" ]; then
-    mkdir -p "$WORDPRESS_DIR"
+mkdir -p "$WORDPRESS_DIR"
 
-    echo "${GREEN}Descargando WordPress...${NC}"
-    curl -sSL "$WORDPRESS_URL" -o /tmp/wordpress.tar.gz
+echo "${GREEN}Descargando WordPress...${NC}"
+curl -sSL "$WORDPRESS_URL" -o /tmp/wordpress.tar.gz
 
-    echo "${GREEN}Descomprimiendo WordPress...${NC}"
-    tar -xzf /tmp/wordpress.tar.gz -C /tmp
+echo "${GREEN}Descomprimiendo WordPress...${NC}"
+tar -xzf /tmp/wordpress.tar.gz -C /tmp
 
-    echo "${GREEN}Copiando archivos de WordPress a $WORDPRESS_DIR...${NC}"
-    cp -r /tmp/wordpress/* "$WORDPRESS_DIR/"
+echo "${GREEN}Copiando archivos de WordPress a $WORDPRESS_DIR...${NC}"
+cp -r /tmp/wordpress/* "$WORDPRESS_DIR/"
 
-    echo "${GREEN}Limpiando archivos temporales...${NC}"
-    rm -rf /tmp/wordpress /tmp/wordpress.tar.gz
-    rm -rf "$WORDPRESS_DIR/wp-config-sample.php"
+echo "${GREEN}Limpiando archivos temporales...${NC}"
+rm -rf /tmp/wordpress /tmp/wordpress.tar.gz
+rm -rf "$WORDPRESS_DIR/wp-config-sample.php"
 
-    echo "${GREEN}Copiando wp-config.php ...${NC}"
-    cp "$WP_CONFIG_SOURCE" "$WORDPRESS_DIR/wp-config.php"
+echo "${GREEN}Copiando wp-config.php ...${NC}"
+cp "$WP_CONFIG_SOURCE" "$WORDPRESS_DIR/wp-config.php"
 
-    echo "${GREEN}Ajustando permisos...${NC}"
-    chown -R www-data:www-data "$WORDPRESS_DIR"
-    chmod -R 755 "$WORDPRESS_DIR"
+echo "${GREEN}Ajustando permisos...${NC}"
+chown -R www-data:www-data "$WORDPRESS_DIR"
+chmod -R 755 "$WORDPRESS_DIR"
 
-    echo "${GREEN}¡WordPress instalado y configurado correctamente!${NC}"
-else
-    echo "${RED}WordPress ya está instalado. No es necesario ejecutar el script nuevamente.${NC}"
-fi
+echo "${GREEN}¡WordPress instalado y configurado correctamente!${NC}"
 
 exec "$@"
